9617a7b748b7b54b5abb34219cced38b6b12047f
DEVENDRA-5470 <ysdevn@gmail.com>
2026-05-02 17:46:11 +0530

    python-assignment-109

---
+++ 01-Assignment-Variables/02-Update-balance.py
@@ -0,0 +1,13 @@
+# Update balance after deposit 
+current_balance = 10000 
+deposit_balance = 5000
+print(F"Current Balance : {current_balance}")
+print(F"Deposite Balance : {deposit_balance}")
+
+current_balance=current_balance+deposit_balance
+print(f"After Deposite : {current_balance}")
+
+withdraw=200
+print(f"Withdraw amount : {withdraw}")
+current_balance=current_balance-withdraw
+print(f"After Withdraw : {current_balance}")
